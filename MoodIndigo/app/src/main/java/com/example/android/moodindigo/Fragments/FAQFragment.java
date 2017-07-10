5e603bdf3d3f4ddc97b18799cd091479296179ff
Mrunzzz <mrunmayiggt@gmail.com>
2017-07-10 11:29:45 +0530

    Tabs and Drawer updated

---
+++ MoodIndigo/app/src/main/java/com/example/android/moodindigo/Fragments/FAQFragment.java
@@ -0,0 +1,28 @@
+package com.example.android.moodindigo.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.moodindigo.R;
+
+/**
+ * Created by owais on 05/07/17.
+ */
+
+public class FAQFragment extends Fragment {
+    public FAQFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        container.removeAllViews();
+        return inflater.inflate(R.layout.fragment_faq, container, false);
+    }
+}
