d83570c4e3cb01b7723ee4fc1685a8bd6c50c2c2
Mrunzzz <mrunmayiggt@gmail.com>
2017-08-20 00:40:58 +0530

    Started with Retrofit

---
+++ MoodIndigo/app/src/main/java/com/example/android/moodindigo/SearchInterface.java
@@ -0,0 +1,30 @@
+package com.example.android.moodindigo;
+
+import com.example.android.pracapp2.data.Book;
+import com.example.android.pracapp2.data.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by mrunz on 12/6/17.
+ */
+
+public interface SearchInterface {
+
+//    @GET("events/")
+//    Call<List<E>> getEvents();
+//
+//    @GET("news/")
+//    Call<List<E>> getNews();
+//
+//
+//    @GET("user/{id}")
+//    Call<List<E>> checkUserDetails(@Path("id") int id);
+
+
+}
